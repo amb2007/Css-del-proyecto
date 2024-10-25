df0990ae354aec6bb925b8937658800b01dad507
Franco-Pedrazzi <d48457362@alumnos.itr.edu.ar>
2024-10-25 10:57:37 -0300

    solucione un error estetico de los botones login y signup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,19 +4,19 @@ import Login from "./Login";
 import { useState } from 'react';
 import Signup from "./Signup";
 function Navbar() {
-    const [SignupIsOpen,SetSignupIsOpen]=useState(false);
-    const [LoginIsOpen,SetLoginIsOpen]=useState(false);
-    const [IdAcount,SetIdAcount]=useState("")
+    const [SignupIsOpen, SetSignupIsOpen] = useState(false);
+    const [LoginIsOpen, SetLoginIsOpen] = useState(false);
+    const [IdAcount, SetIdAcount] = useState("")
     return (
         <nav>
-              {SignupIsOpen &&  <div className='background' onClick={()=>SetSignupIsOpen(false)}>
-          .
-          </div>}
-          {LoginIsOpen &&  <div className='background' onClick={()=>SetLoginIsOpen(false)}>
-          .
-          </div>}
-          {SignupIsOpen && <Signup send={SetIdAcount}></Signup>}
-          {LoginIsOpen && <Login send={SetIdAcount}></Login>}
+            {SignupIsOpen && <div className='background' onClick={() => SetSignupIsOpen(false)}>
+                .
+            </div>}
+            {LoginIsOpen && <div className='background' onClick={() => SetLoginIsOpen(false)}>
+                .
+            </div>}
+            {SignupIsOpen && <Signup send={SetIdAcount}></Signup>}
+            {LoginIsOpen && <Login send={SetIdAcount}></Login>}
             <img id="logo" src="INCLUIR_RUTA_DE_IMAGEN" ></img>
             <ul>
                 <li><a href="/">Home</a></li>
@@ -24,13 +24,14 @@ function Navbar() {
                 <li><a href="/Categorias">Genders</a></li>
                 <li><a href="/Preguntas">Questions</a></li>
                 <li><a href="/Descripcion">Description</a></li>
-                <li><a href="#">Favorites</a></li>   
+                <li><a href="#">Favorites</a></li>
             </ul>
             <div>
-                <button onClick={()=>{SetSignupIsOpen(true)}} id="login">Signup</button>
-                <button onClick={()=>SetLoginIsOpen(true)} id="register">Login</button>
+                <button onClick={() => SetLoginIsOpen(true)} id="Login">Login</button>
+                <button onClick={() => { SetSignupIsOpen(true) }} id="register">Signup</button>
+
             </div>
-            
+
         </nav>
     );
 }
